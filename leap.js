ddfcf701f6807c325dc968677d5f6010b8b8eb62
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-08-02 10:50:32 +0530

    daily practice

---
+++ leap.js
@@ -0,0 +1,16 @@
+// https://exercism.org/tracks/javascript/exercises/leap/edit
+
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isLeap = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    }
+    return true;
+  }
+  return false;
+};
